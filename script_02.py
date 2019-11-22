1711274b11efb7ded660d80d5f8f67ee1000ef03
ule-boole <zhukov.motion@gmail.com>
2019-11-22 14:11:07 +0300

    Second git

--- script_02.py
+++ script_02.py
@@ -1,16 +1,17 @@
 #script_02.py
 import requests
-
+gif_list = ["fail", "people", "lucky"]
 BASE_URL = "https://api.giphy.com/v1/stickers/random"
-params = dict(
+for gif in gif_list:
+    params = dict(
         api_key="dc6zaTOxFJmzC",
-        tag="python",
+        tag="lucky",
         rating="g",
         fmt='json'
         )
-response = requests.get(BASE_URL, params=params)
-print(response.ok)
-gif_url = response.json()['data']['fixed_height_small_url']
-resp_gif = requests.get(gif_url)
-with open("test.gif", "wb") as f:
-    f.write(resp_gif.content)
+    response = requests.get(BASE_URL, params=params)
+    print(response.ok)
+    gif_url = response.json()['data']['fixed_height_small_url']
+    resp_gif = requests.get(gif_url)
+    with open(gif+".gif", "wb") as f:
+        f.write(resp_gif.content)
